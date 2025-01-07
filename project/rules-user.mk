8634d9c1e8829e27ab2204ed469f44b6e34f63cc
angine04 <me@angine.tech>
2025-01-08 04:20:24 +0800

    feat: add assets into userapp tarball

--- project/rules-user.mk
+++ project/rules-user.mk
@@ -5,9 +5,12 @@ $(OBJDIR)user/%: $(OBJDIR)user/%.c.obj $(USER_DEPS_OBJECTS) $(LIBRT_FILE) $(LIBG
 	@$(LD) $(LDFLAGS) -o $@ $^
 	@$(call end-job,done,ld,$(notdir $@))
 
-# archive user programs
-$(USER_TAR_FILE): $(USER_PROG_FILES)
+USER_ASSETS := $(wildcard assets/*)
+
+# archive user programs and assets
+$(USER_TAR_FILE): $(USER_PROG_FILES) $(USER_ASSETS)
 	@$(call begin-job,tar,$(notdir $@))
 	@mkdir -p $(@D)
 	@tar -vcf $@ -C $(OBJDIR)user $(notdir $(USER_PROG_FILES)) > /dev/null 2>&1
+	@tar -rvf $@ -C assets $(notdir $(USER_ASSETS)) > /dev/null 2>&1
 	@$(call end-job,done,tar,$(notdir $@))
